e41ef701ce51a1d7c85ab41b9dd5a335da69c08f
JodliDev <jodlidev@gmail.com>
2025-01-06 14:38:12 +0100

    Reload frontend when server settings arrive

--- src/frontend/ts/network/messageActions/ServerSettingsMessageAction.ts
+++ src/frontend/ts/network/messageActions/ServerSettingsMessageAction.ts
@@ -1,10 +1,11 @@
 import {BaseFrontendMessageAction} from "../BaseFrontendMessageAction";
 import {Site} from "../../views/Site";
-import {SessionLoginMessage} from "../../../../shared/messages/SessionLoginMessage";
 import {ServerSettingsMessage} from "../../../../shared/messages/ServerSettingsMessage";
+import m from "mithril";
 
 export class ServerSettingsMessageAction extends BaseFrontendMessageAction<ServerSettingsMessage> {
 	exec(site: Site): void {
 		site.serverSettings = this.data.serverSettings
+		m.redraw()
 	}
 }
