c094729ed244e9655886c06fd7b3eafa9e87f346
JodliDev <jodlidev@gmail.com>
2025-01-06 09:18:56 +0100

    Fix automatically created folder structure

--- create_folder_structure.js
+++ create_folder_structure.js
@@ -1,14 +1,14 @@
 const {mkdirSync, existsSync} = require("node:fs");
 const { resolve } = require('path');
 
-const backendFolder = resolve(__dirname, "../", "dist", "backend");
+const backendFolder = resolve(__dirname, "./", "dist", "backend");
 if(!existsSync(backendFolder))
 	mkdirSync(backendFolder, { recursive: true });
 
-const frontendFolder = resolve(__dirname, "../", "dist", "frontend");
+const frontendFolder = resolve(__dirname, "./", "dist", "frontend");
 if(!existsSync(frontendFolder))
 	mkdirSync(frontendFolder, { recursive: true });
 
-const configFolder = resolve(__dirname, "../", "dist", "config");
+const configFolder = resolve(__dirname, "./", "dist", "config");
 if(!existsSync(configFolder))
 	mkdirSync(configFolder, { recursive: true });
